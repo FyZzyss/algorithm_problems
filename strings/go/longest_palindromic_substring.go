84d125a80058a6d2accea3d5d90910b964f71d28
p.kutsubin <p.kutsubin@vk.team>
2022-05-03 23:11:49 +0300

    Add go implementations

---
+++ strings/go/longest_palindromic_substring.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func longestSubstring(s string) (res string) {
+	if s == "" {
+		return res
+	}
+	for i := range s {
+		tmp := helper(s, i, i)
+		if len(tmp) > len(res) {
+			res = tmp
+		}
+		tmp = helper(s, i, i+1)
+		if len(tmp) > len(res) {
+			res = tmp
+		}
+	}
+	return res
+}
+
+func helper(s string, left, right int) (res string) {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left = left - 1
+		right = right + 1
+	}
+	return s[left+1 : right]
+}
+
+func main() {
+	fmt.Println(longestSubstring("babad")) // bab
+	fmt.Println(longestSubstring("cbbd"))  // bb
+	fmt.Println(longestSubstring(""))      // ""
+	fmt.Println(longestSubstring("a"))     // a
+}
---
+++ strings/go/longest_substring_without_repeat.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func longestSubstringWithoutRepeat(s string) (res string) {
+	if s == "" {
+		return res
+	}
+	sSlice := strings.Split(s, "")
+	start, finish, maxLength, tmpStart := 0, 0, 0, 0
+	visited := make(map[string]int)
+	for i := range s {
+		if val, ok := visited[sSlice[i]]; ok && start <= val {
+			tmpStart = visited[sSlice[i]] + 1
+		} else {
+			tmpLength := i - tmpStart + 1
+			if maxLength < tmpLength {
+				finish = i
+				start = tmpStart
+				maxLength = tmpLength
+			}
+		}
+		visited[sSlice[i]] = i
+	}
+	return s[start : finish+1]
+}
+
+func main() {
+	fmt.Println(longestSubstringWithoutRepeat("abcabcbb")) // abc
+	fmt.Println(longestSubstringWithoutRepeat("bbbbb"))    // b
+	fmt.Println(longestSubstringWithoutRepeat("pwwkew"))   // "wke"
+}
