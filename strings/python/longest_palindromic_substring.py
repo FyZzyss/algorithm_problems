903db975dd3237e294079a5323af656362a19a1b
FyZzyss <38393787+FyZzyss@users.noreply.github.com>
2024-02-25 00:08:16 +0300

    add longest_palindromic_substring manacher solution

--- strings/python/longest_palindromic_substring.py
+++ strings/python/longest_palindromic_substring.py
@@ -25,6 +25,30 @@ def helper(s: str, left: int, right: int):
     return s[left + 1: right]
 
 
+def longest_substring_manacher(s: str) -> str:
+    if len(s) <= 1:
+        return s
+    start = 0
+    max_str = s[0]
+    max_len = 1
+    s = '#' + '#'.join(s) + '#'
+    dp = [0] * len(s)
+    center = radius = 0
+    for i in range(len(s)):
+        if i < radius:
+            dp[i] = min(radius - i, dp[2 * center - i])
+        while i - dp[i] - 1 >= 0 and i + dp[i] + 1 < len(s) and s[i - dp[i] - 1] == s[i + dp[i] + 1]:
+            dp[i] += 1
+        if i + dp[i] > radius:
+            center = i
+            radius = i + dp[i]
+        if dp[i] > max_len:
+            max_len = dp[i]
+            start = i
+    max_str = s[start - dp[start]: start + dp[start] + 1].replace('#', '') if max_len > 1 else max_str
+    return max_str
+
+
 print(longest_substring('babad'))  # bab
 print(longest_substring('cbbd'))  # bb
 print(longest_substring(None))  # None
